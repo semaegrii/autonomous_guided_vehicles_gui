02fc81d7fa15d3fa9aef12a6edeea72582a80dd7
semaegrii <egrisemaa@gmail.com>
2022-08-14 03:14:15 +0300

    GUI files

--- docs/ui_SplashWindow.py
+++ docs/ui_SplashWindow.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'SplashWindow.ui'
-#
+##
 # Created by: PyQt5 UI code generator 5.10.1
-#
+##
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
